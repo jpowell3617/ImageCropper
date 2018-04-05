6816dee353748fd79d04376b3b2973a1e73df30f
jpowell3617 <jpowell3617@g.fmarion.edu>
2018-04-04 21:50:00 -0400

    Enhance: Fixed the issues causing the Xs and Ys to lose lose their reference and added a bounding box for cropping.

--- ImageCropper/src/app/image-cropper/image-cropper.component.ts
+++ ImageCropper/src/app/image-cropper/image-cropper.component.ts
@@ -10,12 +10,36 @@ export class ImageCropperComponent implements OnInit {
 
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
-  x: number = 50;
-  y: number = 50;
+  x: number = 0;
+  y: number = 0;
   img = new Image();
-  keyInput: cKeyboardInput;
+  w: number = 1000;
+  h: number = 500;
 
   constructor() {
+    document.addEventListener('keydown', (event) => {
+      //Move img up if 'w' or up arrow is pressed
+      if (event.keyCode == 38 || event.keyCode == 87)
+      {
+        this.y -= 2;
+        //this.w += 20;
+      }
+      //Move img down if 's' or down arrow is pressed
+      else if (event.keyCode == 40 || event.keyCode == 83)
+      {
+        this.y += 2;
+      }
+      //Move img left if 'a' or left arrow is pressed
+      else if (event.keyCode == 37 || event.keyCode == 65)
+      {
+        this.x -= 2;
+      }
+      //Move img right if 'd' or right arrow is pressed
+      else if (event.keyCode == 39 || event.keyCode == 68)
+      {
+        this.x += 2;
+      }
+    });
   }
 
 
@@ -24,44 +48,16 @@ export class ImageCropperComponent implements OnInit {
     this.canvas = <HTMLCanvasElement>document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Nicolas_Cage_2011_CC.jpg/220px-Nicolas_Cage_2011_CC.jpg";
-
+    this.ctx.fillStyle = "#000000";
 
     this.ctx.drawImage(this.img, 0, 0);
    
 
     this.img.onload = () => {
-     /*
-      this.ctx.drawImage(img, 0, 0);
-
-      this.ctx.beginPath();
-      this.ctx.lineWidth = 5;
-      this.ctx.strokeStyle = "White";
-      this.ctx.rect(25, 25, 100, 100);
-      this.ctx.stroke();
-
-
-      this.ctx.translate(200, 0);
-      */
-
-      this.keyInput = new cKeyboardInput();
-
-      // PRESS LEFT ARROW OR 'A' KEY
-      this.keyInput.addKeycodeCallback(37, this.imgLeft);
-      this.keyInput.addKeycodeCallback(65, this.imgLeft);
-
-      // PRESS UP ARROW OR 'W' KEY
-      this.keyInput.addKeycodeCallback(38, this.imgUp);
-      this.keyInput.addKeycodeCallback(87, this.imgUp);
-
-      // PRESS RIGHT ARROW OR 'D' KEY
-      this.keyInput.addKeycodeCallback(39, this.imgRight);
-      this.keyInput.addKeycodeCallback(68, this.imgRight);
-
-      // PRESS DOWN ARROW OR 'S' KEY
-      this.keyInput.addKeycodeCallback(40, this.imgDown);
-      this.keyInput.addKeycodeCallback(83, this.imgDown);
 
       this.gameLoop();
+
+      
     }
 
   }
@@ -69,92 +65,29 @@ export class ImageCropperComponent implements OnInit {
   
 
   gameLoop(): void {
-    requestAnimationFrame(this.gameLoop.bind(this);
-    this.keyInput.inputLoop();
+    requestAnimationFrame(this.gameLoop.bind(this));
+    //this.keyInput.inputLoop();
 
     this.ctx.fillStyle = "black";
     this.ctx.fillRect(0, 0, 1280, 720);
+
+    
 
     this.ctx.save();
     this.ctx.translate(this.x, this.y);
     this.ctx.drawImage(this.img, 0, 0, this.img.width, this.img.height);
     this.ctx.restore();
+
+    this.drawBoundingBox();
+
   }
 
-  keyboardInput(event: KeyboardEvent) {
-    // PRESS LEFT ARROW
-    if (event.keyCode == 37) {
-      window.alert("Left Key Pressed");
-    }
-    // PRESS UP ARROW
-    else if (event.keyCode == 38) {
-      window.alert("Up Key Pressed");
-    }
-    // PRESS RIGHT ARROW
-    else if (event.keyCode == 39) {
-      window.alert("Right Key Pressed");
-    }
-    // PRESS DOWN ARROW
-    else if (event.keyCode == 40) {
-      window.alert("Down Key Pressed");
-    }
-    // PRESS SPACE BAR
-    else if (event.keyCode == 32) {
-      window.alert("Space Key Pressed");
-    }
-  }
 
-  imgUp(): void {
-    this.img.y -= 2;
-  }
-
-  imgDown(): void {
-    this.y += 2;
-  }
-
-  imgLeft(): void {
-    this.x -= 2;
-  }
-
-  imgRight(): void {
-    this.x += 2;
-  }
-
-}
-class cKeyboardInput {
-  public keyCallback: { [keycode: number]: () => void; } = {};
-  public keyDown: { [keycode: number]: boolean; } = {};
-  constructor() {
-    document.addEventListener('keydown', this.keyboardDown);
-    document.addEventListener('keyup', this.keyboardUp);
-  }
-
-  public addKeycodeCallback = (keycode: number, f: () => void): void => {
-    this.keyCallback[keycode] = f;
-    this.keyDown[keycode] = false;
-  }
-
-  public keyboardDown = (event: KeyboardEvent): void => {
-    if (this.keyCallback[event.keyCode] != null) {
-      event.preventDefault();
-    }
-    this.keyDown[event.keyCode] = true;
-  }
-
-  public keyboardUp = (event: KeyboardEvent): void => {
-    this.keyDown[event.keyCode] = false;
-  }
-
-  public inputLoop = (): void => {
-    for (var key in this.keyDown) {
-      var is_down: boolean = this.keyDown[key];
-
-      if (is_down) {
-        var callback: () => void = this.keyCallback[key];
-        if (callback != null) {
-          callback();
-        }
-      }
-    }
+  drawBoundingBox(): void {
+    this.ctx.beginPath();
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = "White";
+    this.ctx.rect(400, 100, 250, 250);
+    this.ctx.stroke();
   }
 }
